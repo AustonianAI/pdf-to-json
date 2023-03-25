451d587b868ba62729fb2a1096f46076ddeb6827
Austin Johnson <austin@austinjohnson.me>
2023-03-25 11:19:01 -0500

    something up with vscode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,14 +26,14 @@ export default function Home() {
       <main>
         <div className="max-w-3xl mx-auto sm:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-400">
-            Generate Structured Data
+            JSONIFY - Generate Structured Data
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Convert a PDF to JSON
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Use this handy tool to JSONIFY generate structured data from an
-            uploaded PDF file using your own data schema.
+            Use this handy tool to generate structured data from an uploaded PDF
+            file using your own data schema.
           </p>
         </div>
         <div className="py-2">
